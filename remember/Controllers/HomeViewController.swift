2bd8847da1e05e223061668ecda728cc9fef171d
Kaeli Lo <puiesabu@gmail.com>
2014-10-09 12:30:10 +0800

    Add record button with auto layout

---
+++ remember/Controllers/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  remember
+//
+//  Created by Kaeli Lo on 9/10/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
