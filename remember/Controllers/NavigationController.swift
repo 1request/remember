20b1598681ea69bcc4968cb0a22933e3854f4d38
Kaeli Lo <puiesabu@gmail.com>
2014-10-09 11:48:38 +0800

    Add Navigation Bar

---
+++ remember/Controllers/NavigationController.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationController.swift
+//  remember
+//
+//  Created by Kaeli Lo on 9/10/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
