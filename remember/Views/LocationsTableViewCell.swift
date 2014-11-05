89029b26b2f569da7afbc91921ab2406f34e8874
josephktcheung <josephktcheung@gmail.com>
2014-11-05 15:07:28 +0800

    Set location cell to be swipeable, delete location

--- remember/Views/LocationsTableViewCell.swift
+++ remember/Views/LocationsTableViewCell.swift
@@ -8,32 +8,62 @@
 
 import UIKit
 
-class LocationsTableViewCell: UITableViewCell {
+class LocationsTableViewCell: SwipeableTableViewCell {
 
-    @IBOutlet weak var radioButton: RadioButton!
-    @IBOutlet weak var locationNameLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let radioButton = RadioButton()
+    let locationNameLabel = UILabel()
+
+    override func commonInit() {
+        super.commonInit()
+        dataSource = self
+        makeLayout()
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func makeLayout() {
+        radioButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        locationNameLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
 
-        // Configure the view for the selected state
+        customContentView.addSubview(radioButton)
+        customContentView.addSubview(locationNameLabel)
+
+        radioButton.backgroundColor = UIColor.clearColor()
+
+        let viewsDict = ["radioButton": radioButton, "locationNameLabel": locationNameLabel]
+        let metricsDict = ["radioButtonLeftMargin": 20, "radioButtonWidth": 24, "radioButtonRightMargin": 16, "locationNameLabelRightMargin": 16]
+
+        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("|-radioButtonLeftMargin-[radioButton(radioButtonWidth)]-radioButtonRightMargin-[locationNameLabel]-locationNameLabelRightMargin-|", options: NSLayoutFormatOptions.AlignAllCenterY, metrics: metricsDict, views: viewsDict)
+
+        let radioButtonHeightConstraint = NSLayoutConstraint(item: radioButton, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: radioButton, attribute: NSLayoutAttribute.Width, multiplier: 1.0, constant: 0.0)
+
+        let radioButtonCenterYConstraint = NSLayoutConstraint(item: radioButton, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: customContentView, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0)
+
+        customContentView.addConstraints(horizontalConstraints)
+        customContentView.addConstraints([radioButtonHeightConstraint, radioButtonCenterYConstraint])
     }
-    
+
     func checkRadioButton() {
         radioButton.setChecked(true)
     }
-    
+
     func uncheckedRadioButton() {
         radioButton.setChecked(false)
     }
-    
+
     func isChecked() -> Bool {
         return radioButton._checked
     }
-    
+}
+
+extension LocationsTableViewCell: SwipeableTableViewCellDataSource {
+    func numberOfButtonsInSwipeableCell(cell: SwipeableTableViewCell) -> Int {
+        return 2
+    }
+
+    func swipeableCell(cell: SwipeableTableViewCell, backgroundImageForButtonAtIndex index: Int) -> UIImage? {
+        if index == 0 {
+            return UIImage(named: "trash")
+        } else {
+            return UIImage(named: "edit")
+        }
+    }
 }
