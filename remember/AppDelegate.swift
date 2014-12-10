5dd207e282091eeb1dfcd45b1c3d1caf0fa68bb4
josephktcheung <josephktcheung@gmail.com>
2014-12-10 12:43:34 +0800

    Show remote notification as alert

--- remember/AppDelegate.swift
+++ remember/AppDelegate.swift
@@ -79,6 +79,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             AudioServicesPlaySystemSound(1007)
         }
     }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        let message = userInfo["aps"] as [NSObject: AnyObject]
+        if let alert = message["alert"] as? String {
+            let alertController = UIAlertController(title: "", message: alert, preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            if let rootViewController = window?.rootViewController as? UINavigationController {
+                rootViewController.visibleViewController.presentViewController(alertController, animated: true, completion: nil)
+            }
+        }
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
