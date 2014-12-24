7d73144051db7fdc9510437562f0904d4e6f4232
josephktcheung <josephktcheung@gmail.com>
2014-12-24 13:18:07 +0800

    Fix #154 Scroll up frameView when keyboard appears

--- remember/Views/UserInfoView.swift
+++ remember/Views/UserInfoView.swift
@@ -23,12 +23,60 @@ class UserInfoView: PopUpView, UITextFieldDelegate {
         }
     }
     
+    var frameViewTopConstant: CGFloat?
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+    }
+    
+    override func didMoveToWindow() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShowNotification:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHideNotification:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func willMoveToWindow(newWindow: UIWindow?) {
+        if newWindow == nil {
+            NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+            NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        }
+    }
+
+    // MARK: - Notifications
+    
+    func keyboardWillShowNotification(notification: NSNotification) {
+        updateBottomLayoutConstraintWithNotification(notification)
+    }
+    
+    func keyboardWillHideNotification(notification: NSNotification) {
+        updateBottomLayoutConstraintWithNotification(notification)
+    }
+    
+    // MARK: - Private
+    
+    func updateBottomLayoutConstraintWithNotification(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let animationDuration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber).doubleValue
+        let keyboardEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        let convertedKeyboardEndFrame = convertRect(keyboardEndFrame, fromView: window)
+        let rawAnimationCurve = (notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as NSNumber).unsignedIntValue << 16
+        let animationCurve = UIViewAnimationOptions.init(UInt(rawAnimationCurve))
+        let kbHeight = CGRectGetMaxY(bounds) - CGRectGetMinY(convertedKeyboardEndFrame)
+        let diff = kbHeight - (frame.height - frameView.frame.height - frameViewTopConstant!)
+        
+        if kbHeight > 0 {
+            frameViewTopConstraint?.constant = frameViewTopConstant! - diff
+        } else {
+            frameViewTopConstraint?.constant = frameViewTopConstant!
+        }
+        
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: .BeginFromCurrentState | animationCurve, animations: {
+            self.layoutIfNeeded()
+            }, completion: nil)
     }
     
     lazy var cameraButton: UIButton! = {
@@ -73,6 +121,8 @@ class UserInfoView: PopUpView, UITextFieldDelegate {
         
         overlayView.addSubview(activityView)
         overlayView.addSubview(loadingLabel)
+        
+        frameViewTopConstant = frameViewTopConstraint?.constant
         
         let cameraButtonCenterXConstraint = NSLayoutConstraint(item: cameraButton, attribute: .CenterX, relatedBy: .Equal, toItem: frameView, attribute: .CenterX, multiplier: 1, constant: 0)
         let cameraButtonCenterYConstraint = NSLayoutConstraint(item: cameraButton, attribute: .CenterY, relatedBy: .Equal, toItem: frameView, attribute: .CenterY, multiplier: 0.5, constant: 0)
